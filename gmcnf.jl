20fd18f5bf2b0601cce0fe21e988685b3ef5fc26
Will Usher <william.usher@ouce.ox.ac.uk>
2019-01-04 14:22:07 +0000

    Seems to be working

--- gmcnf.jl
+++ gmcnf.jl
@@ -12,7 +12,7 @@ Masssachusett Institute of Technology, 2013.
 """
 
 function make_edge_dict(edge_nodes, other_nodes)
-    edges = Dict()
+    edges = Dict{Int8,Array{Int8}}()
     for node in Set(edge_nodes)
         for i in eachindex(edge_nodes)
             if edge_nodes[i] == node
@@ -28,7 +28,7 @@ function make_edge_dict(edge_nodes, other_nodes)
 end
 
 
-function gmcnf(; verbose = true)
+function formulate_gmcnf(; verbose = true)
     nodes = ["household", "power_station", "water_plant", "diesel_resource"]
     edges = [2 3; 2 1; 3 1; 4 2;]
     commodities = ["water", "electricity", "diesel"]
@@ -37,9 +37,9 @@ function gmcnf(; verbose = true)
     sink_nodes = view(edges, :, 2)
 
     outflow_edges = make_edge_dict(source_nodes, sink_nodes)
-    @test outflow_edges == Dict{Any,Any}(4=>[2],2=>[3, 1],3=>[1])
+    @test outflow_edges == Dict{Int8, Array{Int8}}(4=>[2], 2=>[3, 1], 3=>[1])
     inflow_edges = make_edge_dict(sink_nodes, source_nodes)
-    @test inflow_edges == Dict{Any,Any}(3=>[2],1=>[2, 3], 2=>[4])
+    @test inflow_edges == Dict{Int8, Array{Int8}}(3=>[2], 1=>[2, 3], 2=>[4])
 
     num_nodes = length(nodes)
     num_edges = length(edges)
@@ -47,19 +47,38 @@ function gmcnf(; verbose = true)
     
     # demand at node by commodity
     demand = zeros((num_nodes, num_comm))
-    demand[1, 1] = 3
-    demand[1, 2] = 5
+
+    demand[1, 1] = 3  # household requires 3 water
+    demand[1, 2] = 5  # household requires 5 electricity
 
     inflow_cost = zeros((num_nodes, num_nodes, num_comm))
     outflow_cost = zeros((num_nodes, num_nodes, num_comm))
 
+    outflow_cost[4, 2, 3] = 0.20  # diesel costs 0.20 £/kWh
+    outflow_cost[2, 3, 2] = 0.03  # water production costs 0.03 £/l
+    outflow_cost[2, 1, 2] = 0.01  # electricity distribution costs 0.01 £/kWh
+    outflow_cost[2, 3, 2] = 0.01  # electricity distribution costs 0.01 £/kWh
+
+
+    # describe the commodity requirements for an in- or out-flow
     requirements_outflow = zeros((num_nodes, num_nodes, num_comm, num_comm))
     requirements_inflow = zeros((num_nodes, num_nodes, num_comm, num_comm))
+
+    
+    # describe the flow gain/loss or transformation between commodities
     transformation = zeros((num_nodes, num_nodes, num_comm, num_comm))
     concurrent_outflow = zeros((num_nodes, num_nodes, num_comm))
     concurrent_inflow = zeros((num_nodes, num_nodes, num_comm))
+    
+    transformation[4, 2, 3, 2] = 3.0  # power plant requires 3 diesel to produce 1 electricity
+    transformation[4, 2, 3, 3] = 1.0
+    transformation[4, 2, 2, 2] = 1.0
+    requirements_outflow[4, 2, 3, 3] = 1.0  # diesel is produced from diesel resource
+    requirements_outflow[2, 3, 2, 1] = 0.2  # water plant requires 0.2 electricity to produce 1 water
+    requirements_outflow[2, 3, 2, 2] = 1.0
+    requirements_outflow[2, 3, 1, 1] = 1.0
 
-    flow_bounds = zeros((num_nodes, num_nodes, num_comm))
+    flow_bounds = fill(999, (num_nodes, num_nodes, num_comm))
 
     model = Model(with_optimizer(GLPK.Optimizer))
 
@@ -83,53 +102,79 @@ function gmcnf(; verbose = true)
         )
         
 
-    requirements_outflow_const = @expression(model,
-                [i=1:num_nodes, k=1:num_comm],
-                if haskey(outflow_edges, i)
-                    sum(sum(requirements_outflow[i, j, k, l] for l in 1:num_comm) * outflow[i, j, k] for j in outflow_edges[i])
-                else
-                    0
-                end)
+    requirements_outflow_const = @expression(
+        model,
+        requirements_outflow_const[i=1:num_nodes, k=1:num_comm],
+        if haskey(outflow_edges, i)
+            
+            sum(
+                sum(requirements_outflow[i, j, k, l] for l in 1:num_comm) * outflow[i, j, k]
+                for j in outflow_edges[i]
+            )
+                
+        else
+            println("No outflow edge found for $(i)")
+            0
+        end
+    )
 
-    requirements_inflow_const = @expression(model,
-                [i=1:num_nodes, k=1:num_comm],
-                if haskey(inflow_edges, i)
-                    sum(sum(requirements_inflow[h, i, m, k] for m in 1:num_comm) * inflow[h, i, k] for h in inflow_edges[i])
-                else
-                    0
-                end)
+    requirements_inflow_const = @expression(
+        model,
+        requirements_inflow_const[i=1:num_nodes, k=1:num_comm],
+        if haskey(inflow_edges, i)
+            sum(
+                sum(requirements_inflow[h, i, m, k] for m in 1:num_comm) * inflow[h, i, k]
+                for h in inflow_edges[i]
+            )
 
-    mass_balance = @constraint(model,
-                               [i=1:num_nodes, k=1:num_comm],
-                               requirements_outflow_const[i, k] - requirements_inflow_const[i, k]
-                               <= demand[i, k])
+        else
+            println("No inflow edge found for $(i)")
+            0
+        end
+    )
 
-    flow_transformation = @constraint(model,
-                [i in keys(outflow_edges), j in outflow_edges[i], k in 1:num_comm],
-                sum(transformation[i, j, k, l] * outflow[i, j, l] for l in 1:num_comm) 
-                == inflow[i, j, k])
+    mass_balance = @constraint(
+        model,
+        mass_balance[i=1:num_nodes, k=1:num_comm],
+        requirements_outflow_const[i, k] - requirements_inflow_const[i, k]
+        <= demand[i, k])
 
-    self_constraint_outflow = @constraint(model,
-                [i in keys(outflow_edges), j in outflow_edges[i], k in 1:num_comm],
-                concurrent_outflow[i, j, k] * outflow[i, j, k] <= 0)
+    flow_transformation = @constraint(
+        model,
+        flow_transformation[i in keys(outflow_edges), j in outflow_edges[i], k in 1:num_comm],
+        sum(transformation[i, j, k, l] * outflow[i, j, l] for l in 1:num_comm) 
+        == inflow[i, j, k])
 
-    self_constraint_inflow = @constraint(model,
-                [i in keys(outflow_edges), j in outflow_edges[i], k in 1:num_comm],
-                concurrent_inflow[i, j, k] * inflow[i, j, k] <= 0)
+    self_constraint_outflow = @constraint(
+        model,
+        self_constraint_outflow[i in keys(outflow_edges), j in outflow_edges[i], k in 1:num_comm],
+        concurrent_outflow[i, j, k] * outflow[i, j, k] <= 0)
 
-    println("Compiled model, now running")
-    JuMP.optimize!(model)
-    println("Finished running, objective: $(JuMP.objective_value(model))")
+    self_constraint_inflow = @constraint(
+        model,
+        self_constraint_inflow[i in keys(outflow_edges), j in outflow_edges[i], k in 1:num_comm],
+        concurrent_inflow[i, j, k] * inflow[i, j, k] <= 0)
 
-    @test JuMP.termination_status(model) == MOI.OPTIMAL
-    @test JuMP.primal_status(model) == MOI.FEASIBLE_POINT
-    # @test JuMP.objective_value(model) == 225700.0
+    return model
+end
 
-    print(mass_balance)
-    @show model
+function populate_gmncf(model::JuMP.Model)
+
+    println(model[:mass_balance])
+    println(model[:flow_transformation])
 
 end
 
-@time gmcnf(verbose = true)
+@time model = formulate_gmcnf(verbose = true)
+@time populate_gmncf(model)
+
+println("Compiled model, now running")
+JuMP.optimize!(model)
+println("Finished running, objective: $(JuMP.objective_value(model))")
+
+@test JuMP.termination_status(model) == MOI.OPTIMAL
+@test JuMP.primal_status(model) == MOI.FEASIBLE_POINT
+# @test JuMP.objective_value(model) == 225700.0
+
 # @time gmcnf(verbose = true)
 # @time gmcnf(verbose = true)
